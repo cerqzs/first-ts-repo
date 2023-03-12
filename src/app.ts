9df8fc9d2bada4c53631fde1b15884b26f208806
unknown <1222642@isep.ipp.pt>
2023-03-12 19:05:41 +0000

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log("Ola mundo");
+function soma (num1:number , num2:number) : number{
+    let result:number;
+     result =num1 + num2;    
+    return result;
+}
+  let x: number;
+  x = soma (4,5);
+   console.log("Resultado = "+ x )
